e728944440cd612530cedc3c6e851d729d4a282b
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2020-11-09 18:36:40 +0100

    updated deploy script to use it in a different env

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-echo "[INFO] switch to deployment branch and rebase from master"
-git checkout gh-pages
-git rebase master
 echo "[INFO] Prod-Build"
 ng build --prod --base-href https://morpfl.github.io/geoapp/
 sudo ngh -e "moritz.pfluegner45@gmail.com" -n "Moritz Pflügner" --dir=dist/geo-app
-echo "[INFO] switch back to master"
-git checkout master
+
 
